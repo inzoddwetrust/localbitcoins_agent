c6b0a1f5932aa5d57cdba5e107508feab0aaa725
inzoddwetrust <inzoddwetrust@gmail.com>
2017-04-17 07:20:41 +0600

    Google rate added

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 source "https://rubygems.org"
 
 gem "sinatra"
+gem 'money'
+gem 'google_currency'
 gem "localbitcoins", :path => "~/ruby/localbitcoins"
--- app.rb
+++ app.rb
@@ -4,6 +4,8 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'localbitcoins'
 require 'time'
+require 'money'
+require 'money/bank/google_currency'
 
 def get_offers_buy currency
 	@client.online_buy_ads_lookup(currency: currency, countrycode: '', country_name: '', payment_method: '').to_hash["data"]["ad_list"]
@@ -14,12 +16,14 @@ def get_offers_sell currency
 end
 
 before do
+	Money::Bank::GoogleCurrency.ttl_in_seconds = 600
+	Money.default_bank = Money::Bank::GoogleCurrency.new
 	@client = LocalBitcoins.new
 end
 
 get '/' do
 	@ticker1 = get_offers_buy ('RUB')
 	@ticker2 = get_offers_sell ('CNY')
-
+	@google_rate = Money.new(1_00, "CNY").exchange_to(:RUB)
 	erb :index
 end
